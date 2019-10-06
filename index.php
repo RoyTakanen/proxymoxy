056425838d589cde4c750d6d254f53a8845f64b2
Kaikkitietokoneista.net <52327670+kaikkitietokoneista@users.noreply.github.com>
2019-10-06 15:01:03 +0300

    Disabled local file reading

--- index.php
+++ index.php
@@ -43,8 +43,8 @@
 		//Tee curl resurssi
 		$ch = curl_init();
 
-		//Aseta url GET datana vastaanotetuksi ja str_replacella poistetaan lokaalien tiedostojen lukeminen
-		curl_setopt($ch, CURLOPT_URL, str_replace( "file://", "DISABLED://",$_GET["url"]));
+		//Aseta url GET datana vastaanotetuksi ja str_ireplacella (case insensitive) poistetaan lokaalien tiedostojen lukeminen
+		curl_setopt($ch, CURLOPT_URL, str_ireplace( "file://", "Kielletty://",$_GET["url"]));
 
 		//Palauta siirto stringinä
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
