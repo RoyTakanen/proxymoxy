0c590b4fec2bf80967d463f9cf1d668dc7b3a18d
Kaikkitietokoneista.net <info@kaikkitietokoneista.net>
2020-08-15 08:47:29 +0300

    Luotu parempi konfigurointi

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 const http = require('http'); //Otetaan http lisäosa käyttöön
 const validUrl = require('valid-url');
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
 
 const config = {
+  uri: 'http://proxy.kaikkitietokoneista.net',
   http_server_port: process.env.HTTP_SERVER_PORT,
   http_proxy_port: process.env.HTTP_PROXY_PORT
 }
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+//setup public folder
 
-http.createServer(function (req, res) {
-  res.end('Hei')
-}).listen(config.http_server_port)
+app.get('*',function (req, res) {
+  res.render('index', {
+    proxy_port: config.http_proxy_port,
+    web_port: config.http_server_port,
+    uri: config.uri
+  })
+});
+
+app.listen(config.http_server_port);
+
+/* Proxy */
 
 http.createServer(function (req, res) { //Luodaan palvelin vastaanottamaan pyyntöjä
+  console.log(`Käyttäjä IP:stä ${req.connection.remoteAddress} lataa sivua ${req.url}`);
   if (validUrl.isUri(req.url)){
     http.get(req.url, (vastaus) => { //Tehdään pyyntö perustuen käyttäjän luoman pyynnön sisältävään urliin
       let data = '' //Luodaan vain tälle lohkolle oma data muuttuja
