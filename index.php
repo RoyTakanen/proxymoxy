5e5593c5b084f178b6bf6535f2db760c125cbf0f
Kaikkitietokoneista <info@kaikkitietokoneista.net>
2021-10-08 22:18:48 +0300

    Return url content

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     if (isset($_GET["url"])) {
         $url = $_GET["url"];
         if (filter_var($url, FILTER_VALIDATE_URL)) {
-
+            echo file_get_contents($url);
         } else {
             http_response_code(500);
             header('Content-Type: application/json; charset=utf-8');
